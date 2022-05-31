fe8451454add1a13ec3b06bac879c89a5a1c0e76
tylerboy0604 <74601919+tylerboy0604@users.noreply.github.com>
2022-05-31 10:48:36 +0200

    deletedsecond code

--- homepage.php
+++ homepage.php
@@ -2,8 +2,6 @@
 include('DB/DataBase_connection.php');
 
 
-$aanbiedingsql = "SELECT 	titel, begindatum, einddatum, omschrijving, afbeelding FROM aanbiedingen ORDER BY begindatum ASC LIMIT 1;"
-$aanbied1 = $conn->query(aanbiedingsql);
 
 $eventsql = "SELECT evenementen.artiest_id,	evenementen.datum, artiesten.naam FROM evenementen LEFT JOIN artiesten ON evenementen.artiest_id = artiesten.artiest_id ORDER BY evenementen.datum ASC LIMIT 3;";
 $events3 = $conn->query($eventsql);
