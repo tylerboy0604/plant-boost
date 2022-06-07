e93894b591e08484418b3a7b36448df37aac1913
jilk1 <94365522+jilk1@users.noreply.github.com>
2022-06-07 10:41:34 +0200

    contact pagina gemaakt

--- contact.php
+++ contact.php
@@ -53,24 +53,11 @@ $events3 = $conn->query($eventsql);
 
         </section>
 
-        <section id="goup" class="row">
-            <?php
-            while ($evenement = $events3->fetch_assoc()) {
-            ?>
-                <article class="col-1 col-md-1 col-lg-4">
-                    <p>
-                        <?php echo $evenement['datum'] . "<br>" . $evenement['naam']; ?>
-                    </p>
-                    <br>
-                    <img id="image-event" src="artiesten/<?php echo $evenement['artiest_id'] ?>.png">
-                </article>
-
-            <?php
-            }
-
-            ?>
+        <section id="banner">
 
         </section>
+
+
     </main>
     <footer>
 
