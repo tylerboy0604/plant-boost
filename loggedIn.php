3d8a11a5de00504b00f114c850836b5d61e645c8
tylerboy0604 <74601919+tylerboy0604@users.noreply.github.com>
2022-06-24 14:26:46 +0200

    added

--- loggedIn.php
+++ loggedIn.php
@@ -11,7 +11,11 @@
     <title>Home</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<<<<<<< Updated upstream
     <link rel="stylesheet" type="text/css" href="css/loggedin.css   ">
+=======
+    <link rel="stylesheet" type="text/css" href="css/loggedin.css   s">
+>>>>>>> Stashed changes
 </head>
 
 <body>
