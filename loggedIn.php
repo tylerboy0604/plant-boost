f14be14b26538a1d02fc057283c46041e6cf2a7a
tylerboy0604 <74601919+tylerboy0604@users.noreply.github.com>
2022-06-29 10:24:07 +0200

    logged in made

--- loggedIn.php
+++ loggedIn.php
@@ -11,11 +11,7 @@
     <title>Home</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-<<<<<<< Updated upstream
-    <link rel="stylesheet" type="text/css" href="css/loggedin.css   ">
-=======
-    <link rel="stylesheet" type="text/css" href="css/loggedin.css   s">
->>>>>>> Stashed changes
+    <link rel="stylesheet" type="text/css" href="css/Loggedin.css">
 </head>
 
 <body>
@@ -24,9 +20,8 @@
             <img id="image-logo" class=" col-md-1 col-lg-1" src="IMG/logo-with-tekst.png" alt="plant boost logo">
             <nav id="left" class="col col-md-7 col-lg-5 row">
                 <a class="col-md-2 col-2" href="homepage.php">Home</a>
-                <a class="col-md-3 col-3" href="eventpage.php">Evenementen</a>
                 <a class="col-md-2 col-2" href="">Product</a>
-                <a class="col-md-1 col-1" href="">Zoek</a>
+                <a class="col-md-1 col-1" href="">overzicht</a>
                 <a class="col-md-2 col-2" href="">Contacten</a>
                 <a class="col-md-2 col-1" href="">Inloggen</a>
             </nav>
@@ -34,6 +29,41 @@
     </header>
 
 
+    <main>
+        <section id="background">
+            <section>
+                <p>kies wat je wilt aanpassen</p>
+                <select id="select-design">
+                    <option value="aanbiedingen">aanbiedingen</option>
+                    <option value="artiesten">artiesten</option>
+                    <option value="evenementen">events</option>
+                    <option value="verwijderen">verwijder</option>
+                </select>
+            </section>
+            <section>
+                <p class="inputlabel">voeg toe</p>
+                <input class="design" type="text" name="add-item" placeholder="add">
+            </section>
+            <section>
+                <p class="inputlabel">voeg toe</p>
+                <input class="design" type="text" name="add-item" placeholder="add">
+            </section>
+            <section>
+                <p class="inputlabel">voeg toe</p>
+                <input class="design" type="text" name="add-item" placeholder="add">
+            </section>
+            <section>
+                <p class="inputlabel">voeg toe</p>
+                <input class="design" type="text" name="add-item" placeholder="add">
+            </section>
+            <section>
+                <p class="inputlabel">voeg toe</p>
+                <input class="design" type="text" name="add-item" placeholder="add">
+            </section>
+            <button type="button">toevoegen</button>
+        </section>
+
+    </main>
 
 
 
@@ -43,7 +73,7 @@
 
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
 
